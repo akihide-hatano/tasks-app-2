22bdb6092210e207265c374a673fcfc42dd6d8f8
akihide-hatano <a.h.19841202@gmail.com>
2025-10-19 07:45:32 +0900

    TaskFactory.phpを更新しました。

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
@@ -17,7 +18,9 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id'=>User::factory(),
+            'title'=>$this->faker->sentence(3),
+            'is_done'=>false,
         ];
     }
 }
