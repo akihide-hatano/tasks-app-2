293caf7e3c013769dea69d569faf114fbf16f7ab
akihide-hatano <a.h.19841202@gmail.com>
2025-10-19 09:06:15 +0900

    TaskSeeder.phpを更新

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Task;
+use Illuminate\Support\Arr;
 
 class TaskSeeder extends Seeder
 {
