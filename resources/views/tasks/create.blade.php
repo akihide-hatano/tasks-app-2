fb3536861570ba104c4f82e10ed9399499c5c539
akihide-hatano <a.h.19841202@gmail.com>
2025-10-20 21:31:24 +0900

    create.blade.phpにてsessionのメッセージ表示を追加

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl">Create Task</h2>
+    </x-slot>
+
+    <div class="max-w-3xl mx-auto p-6">
+        @if(session('status'))
+            <div class="mb-4 rounded bg-green-100 p-3 text-green-800">
+                {{session('status')}}
+            </div>
+        @endif
+    </div>
+</x-app-layout>
