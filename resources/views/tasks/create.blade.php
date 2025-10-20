a816f83bb23d74f0c855434b40100a1acf42d5a3
akihide-hatano <a.h.19841202@gmail.com>
2025-10-20 21:57:54 +0900

    tasks/create.blade.phpをtitleの部分を"新しいタスクの作成"に変更しました。

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -15,5 +15,18 @@
                 {{ session('error') }}
             </div>
         @endif
+
+        <form action="{{route('tasks.store')}}" method="POST">
+            @csrf
+
+            {{-- Title --}}
+            <div class="mb-4">
+                <label class="block mb-1 font-semibold">
+                    タイトル<span class="text-red-500">*</span>
+                </label>
+                <input type="text" name="title" value="{{old('title',$task->title)}}"
+                    class="w-full border rounded px-3 py-2" maxlength="100" required>
+            </div>
+        </form>
     </div>
 </x-app-layout>
