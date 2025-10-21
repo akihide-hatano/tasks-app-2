82b64842b761ecf408169942b3d58c09ad8ba210
akihide-hatano <a.h.19841202@gmail.com>
2025-10-21 11:34:19 +0900

    StoreTaskRequest,UpdateTaskRequestを修正。

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -22,7 +22,7 @@ class UpdateTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title'=>['required','string','max:100'],
+            'title'=>['sometimes','string','max:100'],
             'is_done'=>['sometimes','boolean'],
         ];
     }
